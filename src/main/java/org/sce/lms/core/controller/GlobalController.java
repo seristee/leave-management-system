5e2bbab2c3c19400c1778f0d41a803b3f554e23f
Sheldon Eristee <sheldon.eristee@gmail.com>
2020-12-27 12:57:15 -0400

    added dashboard

--- src/main/java/org/sce/lms/core/controller/GlobalController.java
+++ src/main/java/org/sce/lms/core/controller/GlobalController.java
@@ -4,7 +4,40 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+@Configuration
+@PropertySource(value = { "classpath:application.properties" })
 public class GlobalController {
     protected Log logger = LogFactory.getLog(this.getClass());
+
+    @Value("${build.version}")
+    protected String buildVersion;
+
+    @ModelAttribute("currrentUser")
+    public String currentUser(){
+        return getPrincipal();
+    }
+
+    protected String getPrincipal() {
+        String userName = null;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            userName = ((UserDetails) principal).getUsername();
+        } else {
+            userName = principal.toString();
+        }
+        return userName;
+    }
+
+    @ModelAttribute("appVersion")
+    public String getBuildVersion() {
+        String[] arr = buildVersion.split("-");
+        return arr[0];
+    }
 }
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,8 +1,20 @@
 // fullcalendar
-document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth'
+// document.addEventListener('DOMContentLoaded', function() {
+//     let calendarEl = document.getElementById('calendar');
+//
+//     let calendar = new FullCalendar.Calendar(calendarEl, {
+//         initialView: 'dayGridMonth',
+//         headerToolbar: {
+//             left: 'prev,next today',
+//             center: 'title',
+//             right: 'dayGridMonth,timeGridWeek,timeGridDay'
+//         },
+//     });
+//     calendar.render();
+// });
+
+$(function () {
+    $('#calendar').fullCalendar({
+
     });
-    calendar.render();
 });
