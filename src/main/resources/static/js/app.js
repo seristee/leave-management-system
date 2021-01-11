8ec4c6d8e8e99bcf72313aa580939e5e4961b123
Sheldon Eristee <sheldon.eristee@gmail.com>
2021-01-10 21:24:00 -0400

    modified

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,18 +2,18 @@ $(function () {
 
     var url = window.location;
     // for single sidebar menu
-    $('ul.nav-sidebar a').filter(function () {
-        return this.href == url;
-    }).addClass('active');
-        //.css('color', 'white');
-
+    // $('ul.nav-sidebar a').filter(function () {
+    //     return this.href == url;
+    // }).addClass('active');
+    //     .css('color', 'white');
+    
     // for sidebar menu and treeview
-    $('ul.nav-treeview a').filter(function () {
-        return this.href == url;
-    }).parentsUntil(".nav-sidebar > .nav-treeview")
-        .css({'display': 'block'})
-        .addClass('menu-open').prev('a')
-        .addClass('active');
+    // $('ul.nav-treeview a').filter(function () {
+    //     return this.href == url;
+    // }).parentsUntil(".nav-sidebar > .nav-treeview")
+    //     .css({'display': 'block'})
+    //     .addClass('menu-open').prev('a')
+    //     .addClass('active');
 
 
 
